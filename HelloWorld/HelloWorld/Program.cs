21bf68e11737a874ac19df70010ed86f859e9342
CrazyCookieBuddy <stockfisch97@gmail.com>
2019-11-27 10:42:47 +0100

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+
     class Program
     {
+        public static string Worldstring = "Ham and Eggs are better than \"Hello World!\"";
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Worldstring);
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
         }
     }
 }
