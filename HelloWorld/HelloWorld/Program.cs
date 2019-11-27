e69183196ed849098dbfcdbe5e92d6f609472a6b
CrazyCookieBuddy <stockfisch97@gmail.com>
2019-11-27 10:45:52 +0100

    Update Program.cs
    added int & bool

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,10 +10,16 @@ namespace HelloWorld
     class Program
     {
         public static string Worldstring = "Ham and Eggs are better than \"Hello World!\"";
+        public static int TheAnswer = 42;
+        public static bool WorldFlat = true;
+
         static void Main(string[] args)
         {
 
             Console.WriteLine(Worldstring);
+            Console.WriteLine("World is flat: " + WorldFlat);
+            Console.WriteLine("Whats the answer to everything? ");
+            Console.WriteLine(TheAnswer);
             Console.ReadKey();
 
 
