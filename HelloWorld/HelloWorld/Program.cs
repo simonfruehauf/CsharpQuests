a8d1b205472579ef73802cb7600dd0b4e545eca8
CrazyCookieBuddy <stockfisch97@gmail.com>
2019-12-11 11:36:43 +0100

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -16,12 +16,127 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Worldstring);
-            Console.WriteLine("World is flat: " + WorldFlat);
-            Console.WriteLine("Whats the answer to everything? ");
-            Console.WriteLine(TheAnswer);
-            Console.ReadKey();
+        }
 
+
+        static void dataTypes()
+        {
+            bool boolean = true;
+            int integer = 1;
+            double doublenumber = 1.00;
+            float floatpoint = 1.11f;
+            string textstring = "random text";
+            char singlechar = 'a';
+            Console.WriteLine("bool " + boolean);   
+            Console.WriteLine("int " + integer);
+            Console.WriteLine("double " + doublenumber);
+            Console.WriteLine("float " + floatpoint);
+            Console.WriteLine("string " + textstring);
+            Console.WriteLine("char " + singlechar);
+        }
+        static void ReadConsoleLine()
+        {
+            string amount = Console.ReadLine();
+            int number;
+
+
+            try
+            {
+                number = Convert.ToInt16(amount);
+                Console.WriteLine(AddOne(number));
+            }
+            catch
+            {
+                Console.WriteLine("Not a number.");
+            }
+
+            Console.ReadLine();
+        }
+        static void Banana()
+        {
+            int sliceAmount = 1;
+            bool sliced = true;
+
+            switch (sliced)
+            {
+                case false:
+                    Console.WriteLine("The banana is not sliced. " + "sliced = " + sliced);
+                    break;
+                case true:
+                    Console.WriteLine("The banana is sliced. " + "sliced = " + sliced);
+                    break;
+            }
+
+            Console.WriteLine("There are " + sliceAmount + " slices.");
+
+        }
+        static int AddOne(int number)
+        {
+            number++;
+            return number;
+        }
+        static void Maths()
+        {
+            int a;
+            int b;
+
+            a = 10;
+            b = 69;
+
+            int c = a + b;
+
+            Console.WriteLine(c);
+
+            float d;
+            float e;
+
+            d = 12.400f;
+            e = 57.44f;
+
+            float f = d - e;
+
+            Console.WriteLine(f);
+
+            int g = a * b;
+            float h = d * e;
+
+            float i = g / h;
+
+            Console.WriteLine(i);
+        }
+        static void Name()
+        {
+            string name;
+            retryName:
+            Console.Write("Please input your name: ");
+            name = Console.ReadLine();
+            if (name=="")
+            {
+                goto retryName;
+
+            }          
+
+            Console.WriteLine("Hello, " + name + "!");
+
+            int age;
+            retryAge:
+            Console.Write("Please input your age as a number: ");
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                goto retryAge;
+            }
+
+            
+            if (name==age.ToString())
+            {
+                Console.WriteLine("Your Name is the same as your age. Interesting.");
+            }
+            Console.WriteLine("Hello, " + name + "! Thank you for telling me your age (which is " + age + ").");
+            Console.ReadLine();
 
 
         }
