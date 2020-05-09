50f3ecdef1088fb387bdbb96b42c98f2fe188943
CrazyCookieBuddy <stockfisch97@gmail.com>
2020-05-09 12:12:46 +0200

    Update Snake.cs
    finished snake with score, speed adjustment & food

--- HelloWorld/HelloWorld/Snake.cs
+++ HelloWorld/HelloWorld/Snake.cs
@@ -11,13 +11,27 @@ namespace HelloNamespace
     //documentation : https://imgur.com/a/h5s702i
     class Snake
     {
-        int foodx, foody, dx, dy;
+        int dx, dy;
         int speed = 500;
         bool running = true;
         ConsoleKeyInfo consoleKey;
+        Dictionary<int, Position> snake = new Dictionary<int, Position>();
+
+        struct Bodypart
+        {
+            public int index;
+            public Position pos;
+
+            public Bodypart(int index_m, Position pos_m)
+            {
+                index = index_m;
+                pos = pos_m; 
+            }
+        }
         char snakechar = 'O';
-        char border = '*';
-        bool trail;
+        char borderchar = 'X';
+        char foodchar = 'ö';
+        bool trail =false;
         struct GameSize
         {
             public int x;
@@ -39,7 +53,8 @@ namespace HelloNamespace
                 y = y_m;
             }
         }
-       
+
+        Position food;
         enum Direction
         {
             Up, Right, Down, Left
@@ -58,10 +73,10 @@ namespace HelloNamespace
         }
 
 
-         public void Play()
+        public void Play()
         {
-            List<Position> snake = new List<Position>();
-            snake.Add(new Position(gm.x / 2, gm.y / 2));
+            food = nextFoodPos();
+            snake.Add(0, new Position (gm.x / 2, gm.y / 2));
             Console.Clear();
             for (int i = 0; i <= gm.x; i++)
             {
@@ -70,18 +85,18 @@ namespace HelloNamespace
                     if (j <= 2 | i >= gm.x | i <= 1 | j >= gm.y)
                     {
                         Console.SetCursorPosition(i, j);
-                        Console.Write(border);
+                        Console.Write(borderchar);
                     }
                 }
             }
             
             Console.SetCursorPosition(0, 0);
-            Console.WriteLine("Arrows move up/down/right/left. 'c' to clear  'esc' to quit.");
+            Console.WriteLine("Arrows change direction, 'esc' to quit. Use '+' and '-' to adjust speed.");
 
             do // until we press escape
             {
                 
-                //Console.SetCursorPosition(snake[0].x, snake[0].y);
+                Console.SetCursorPosition(snake[0].x, snake[0].y);
 
                 // see if a key has been pressed
                 if (Console.KeyAvailable)
@@ -91,10 +106,19 @@ namespace HelloNamespace
                     switch (consoleKey.Key)
                     {
                         case ConsoleKey.J:
-                            snake.Add(new Position(snake.Last<Position>().x - dx, snake.Last<Position>().y - dy));
+                            //AddBodypart();
                             break;
-                        case ConsoleKey.T:
-                            trail = true;
+                        case ConsoleKey.OemPlus:
+                            if (speed > 200)
+                            {
+                                speed -= 100;
+                            }
+                            break;
+                        case ConsoleKey.OemMinus:
+                            if (speed < 700)
+                            {
+                                speed += 100;
+                            }
                             break;
                         case ConsoleKey.C:
                             Console.Clear();
@@ -122,45 +146,132 @@ namespace HelloNamespace
                             break;
                     }
                 }
+                Position temp = new Position(0,0);
+                Position last = new Position(0, 0);
+                for (int i = 0; i < snake.Count; i++)
+                {
 
-                
+                    Console.SetCursorPosition(snake[i].x, snake[i].y);
 
+                    if (!trail)
+                        Console.Write(' ');
+
+                    temp = snake[i];
+                    if (i == 0)
+                    {
+                        last = snake[i];
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        snake[i] = new Position(temp.x + dx, temp.y + dy);
+                        temp = snake[i];
+                        if (snake[i].x > gm.x - 1)
+                            snake[i] = new Position(3, temp.y + dy);
+                        if (snake[i].x < 2)
+                            snake[i] = new Position(gm.x - 1, temp.y + dy);
+                        temp = snake[i];
+
+                        if (snake[i].y > gm.y-1)
+                            snake[i] = new Position(temp.x + dx, 3);
+
+                        if (snake[i].y < 3)
+                            snake[i] = new Position(temp.x + dx, gm.y - 1);
+                        //keep snake in the square
+
+                        // write the character in the new position
+                        Console.SetCursorPosition(snake[i].x, snake[i].y);
+                        Console.Write(snakechar);
+                        Console.ResetColor();
+
+                    }
+                    else if (i !=0)
+                    {
+                        Position last_t = snake[i];
+                        snake[i] = new Position(last.x, last.y);
+                        if (i == 1)
+                        {
+                            //???
+                        }
+                        Console.SetCursorPosition(snake[i].x, snake[i].y);
+                        Console.Write(snakechar);
+                        last = last_t;
+                    }
+                    
+
+                }
+
+                //check for intersections
 
                 for (int i = 0; i < snake.Count; i++)
                 {
-                    Console.SetCursorPosition(snake[i].x, snake[i].y);
-                    
-                    if (trail == false)
-                        Console.Write(' ');
-
-                    Position temp = snake[i];
-                    snake[i] = new Position(temp.x + dx, temp.y + dy);
-                    if (snake[i].x > gm.x-1)
-                        snake[i] = new Position(3, temp.y + dy);
-                    if (snake[i].x < 2)
-                        snake[i] = new Position(gm.x-1, temp.y + dy);
-                    temp = snake[i];
-                    if (snake[i].y > gm.y-1)
-                        snake[i] = new Position(temp.x, 2); 
-                    if (snake[i].y < 3)
-                        snake[i] = new Position(temp.x, gm.y-1);
-                    //keep snake in the square
-
-                    // write the character in the new position
-                    Console.SetCursorPosition(snake[i].x, snake[i].y);
-                    Console.Write(snakechar);
+                    if (i != 0)
+                    {
+                        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y)
+                        {
+                            running = false;
+                            break;
+                        }
+                    }
+                    else if (i == 0 && snake[i].x == food.x && snake[i].y == food.y)
+                    {
+                        //eat
+                        food = nextFoodPos();
+                        AddBodypart();
+                    }
                 }
 
-                //draw borders
+                // draw food
 
-                
-                
+                if (food.x != 0 && food.y != 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.SetCursorPosition(food.x, food.y);
+                    Console.Write(foodchar);
+                    Console.ResetColor();
+
+                }
+
+                Console.SetCursorPosition(gm.x, gm.y);
                 // pause to allow eyeballs to keep up
                 Thread.Sleep(speed);
 
             } while (running);
-            Console.Clear();
-            ChangeCursor(0, 0);
+
+            //Game over
+            PrintScore();
+        }
+
+        Position nextFoodPos()
+        {
+            List<Position> all = new List<Position>();
+
+            for (int i = 2; i < gm.x-1; i++)
+            {
+                for (int j = 4; j < gm.y-1; j++)
+                {
+                    if (!snake.ContainsValue(new Position(i, j)))
+                    {
+                        all.Add(new Position(i, j));
+                    }
+                    
+                }
+            }
+            
+            Random rnd = new Random();
+
+            return all[rnd.Next(0, all.Count-1)];
+        }
+        void AddBodypart()
+        {
+            snake.Add(snake.Count, new Position(snake[snake.Count - 1].x - dx, snake[snake.Count - 1].y - dy));
+        }
+
+        void PrintScore()
+        {
+            string text = "Game over. You have a score of " + snake.Count() + "!";
+
+            Console.SetCursorPosition(gm.x - (text.Length / 2), gm.y / 2);
+            Console.Write(text);
+
+            Console.SetCursorPosition(0, gm.y + 2);
         }
     }
 }
