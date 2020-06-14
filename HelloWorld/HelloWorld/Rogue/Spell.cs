96352c5d8c6adbbceafd82bae4cec0fa097046cb
CrazyCookieBuddy <stockfisch97@gmail.com>
2020-06-14 10:15:44 +0200

    Create Spell.cs
    Added spell class containing possible future spells

---
+++ HelloWorld/HelloWorld/Rogue/Spell.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNamespace.Rogue
+{
+    abstract class Spell
+    {
+        public int cost; //manacost
+        public int range; //range from caster in tiles
+        public bool aoe; //area of effect spell
+        public int width; //width of aoe
+        public bool circle; //true = circle, false = box;
+
+        public int damage; //damage applied
+        public int heal; //health healed
+        abstract public void Cast(Tile caster, int cost);
+    }
+
+    #region Spells
+    class Fireball : Spell
+    {
+        public override void Cast(Tile caster, int cost)
+        {
+            if (caster.player != null)
+            {
+                if (caster.player.UseMana(cost))
+                {
+                    //cast
+
+                }
+            }
+        }
+    }
+    class Teleport : Spell
+    {
+        public override void Cast(Tile caster, int cost)
+        {
+            if (caster.player != null)
+            {
+                if (caster.player.UseMana(cost))
+                {
+                    //cast
+
+                }
+            }
+
+        }
+    }
+    class Blessing : Spell
+    {
+        public override void Cast(Tile caster, int cost)
+        {
+            if (caster.player != null)
+            {
+                if (caster.player.UseMana(cost))
+                {
+                    //cast
+
+                }
+            }
+        }
+    }
+
+    #endregion
+
+}
