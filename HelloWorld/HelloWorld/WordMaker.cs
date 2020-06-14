616cb74a5b4033084bc122e5cf9105741d2a68cb
CrazyCookieBuddy <stockfisch97@gmail.com>
2020-06-14 10:07:01 +0200

    Update WordMaker.cs
    added better word generation (double consonants & vowels, optional consonant & vowel arrays & seed

--- HelloWorld/HelloWorld/WordMaker.cs
+++ HelloWorld/HelloWorld/WordMaker.cs
@@ -8,56 +8,76 @@ namespace HelloNamespace
 {
     class WordMaker
     {
+       static string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+       static string[] vowels = { "a", "e", "i", "o", "u", };
 
-        public string WordFinder(int length)
+        public string WordFinder(int length, int seed = 0, string[] vw = null, string[] cn = null)
         {
-            Random rnd = new Random(DateTime.Now.Millisecond);
-            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
-            string[] vowels = { "a", "e", "i", "o", "u",};
+            Random rnd;
+
+            if (seed == 0)
+            {
+               rnd = new Random(DateTime.Now.Millisecond);
+            }
+            else
+            {
+                rnd = new Random(seed);
+            }
+
+            if (vw == null)
+            {
+                vw = vowels;
+            }
+            if (cn == null)
+            {
+                cn = consonants;
+            }
+
+            string word = "";
 
 
-                string word = "";
-
-
-                // Generate the word in consonant / vowel pairs
-                while (word.Length < length)
+            // Generate the word in consonant / vowel pairs
+            while (word.Length < length)
+            {
+                if (length != 1 || rnd.Next(100) >= 90)
                 {
-                    if (length != 1)
+                    // Add the consonant
+                    string consonant = GetRandomLetter(rnd, cn);
+
+                    if (consonant == "q" && word.Length + 3 <= length) // check +3 because we'd add 3 characters in this case, the "qu" and the vowel.  Change 3 to 2 to allow words that end in "qu"
                     {
-                        // Add the consonant
-                        string consonant = GetRandomLetter(rnd, consonants);
-
-                        if (consonant == "q" && word.Length + 3 <= length) // check +3 because we'd add 3 characters in this case, the "qu" and the vowel.  Change 3 to 2 to allow words that end in "qu"
-                        {
-                            word += "qu";
-                        }
-                        else
-                        {
-                            while (consonant == "q")
-                            {
-                                // Replace an orphaned "q"
-                                consonant = GetRandomLetter(rnd, consonants);
-                            }
-
-                            if (word.Length + 1 <= length)
-                            {
-                                // Only add a consonant if there's enough room remaining
-                                word += consonant;
-                            }
-                        }
+                        word += "qu";
                     }
-
-                    if (word.Length + 1 <= length)
+                    else
                     {
-                        // Only add a vowel if there's enough room remaining
-                        word += GetRandomLetter(rnd, vowels);
+                        while (consonant == "q")
+                        {
+                            // Replace an orphaned "q"
+                            consonant = GetRandomLetter(rnd, cn);
+                        }
+
+                        if (word.Length + 1 <= length)
+                        {
+                            if (word.Length +2 <= length && rnd.Next(100) >= 80)
+                            {
+                                word += consonant + consonant;
+                            }
+                            // Only add a consonant if there's enough room remaining
+                            word += consonant;
+                        }
                     }
                 }
-
-
-
+                if (word.Length + 1 <= length)
+                {
+                    // Only add a vowel if there's enough room remaining
+                    word += GetRandomLetter(rnd, vw);
+                    if (word.Length + 1 <= length && rnd.Next(0, 100) >= 75) //add secondary vowel
+                    {
+                        word += GetRandomLetter(rnd, vw);
+                    }
+                }
+            }
             return word;
-            
         }
 
         private static string GetRandomLetter(Random rnd, string[] letters)
